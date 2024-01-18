be527231a17cf68f07f26a932bc183863228a3ce
Nutto <91049342+Nutmito@users.noreply.github.com>
2024-01-18 14:53:06 +0700

    ಥ_ಥ

---
+++ Tic Tac Toe/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Tic_Tac_Toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            A1 = new Button();
+            A2 = new Button();
+            A3 = new Button();
+            B1 = new Button();
+            B2 = new Button();
+            B3 = new Button();
+            C1 = new Button();
+            C2 = new Button();
+            C3 = new Button();
+            NewGame = new Button();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // A1
+            // 
+            A1.Location = new Point(12, 12);
+            A1.Name = "A1";
+            A1.Size = new Size(100, 100);
+            A1.TabIndex = 0;
+            A1.UseVisualStyleBackColor = true;
+            A1.Click += A1_Click;
+            A1.MouseEnter += button_enter;
+            A1.MouseLeave += button_leave;
+            // 
+            // A2
+            // 
+            A2.Location = new Point(118, 12);
+            A2.Name = "A2";
+            A2.Size = new Size(100, 100);
+            A2.TabIndex = 1;
+            A2.UseVisualStyleBackColor = true;
+            A2.Click += A2_Click;
+            A2.MouseEnter += button_enter;
+            A2.MouseLeave += button_leave;
+            // 
+            // A3
+            // 
+            A3.Location = new Point(224, 12);
+            A3.Name = "A3";
+            A3.Size = new Size(100, 100);
+            A3.TabIndex = 2;
+            A3.UseVisualStyleBackColor = true;
+            A3.Click += A3_Click;
+            A3.MouseEnter += button_enter;
+            A3.MouseLeave += button_leave;
+            // 
+            // B1
+            // 
+            B1.Location = new Point(12, 118);
+            B1.Name = "B1";
+            B1.Size = new Size(100, 100);
+            B1.TabIndex = 3;
+            B1.UseVisualStyleBackColor = true;
+            B1.Click += B1_Click;
+            B1.MouseEnter += button_enter;
+            B1.MouseLeave += button_leave;
+            // 
+            // B2
+            // 
+            B2.Location = new Point(118, 118);
+            B2.Name = "B2";
+            B2.Size = new Size(100, 100);
+            B2.TabIndex = 4;
+            B2.UseVisualStyleBackColor = true;
+            B2.Click += B2_Click;
+            B2.MouseEnter += button_enter;
+            B2.MouseLeave += button_leave;
+            // 
+            // B3
+            // 
+            B3.Location = new Point(224, 118);
+            B3.Name = "B3";
+            B3.Size = new Size(100, 100);
+            B3.TabIndex = 5;
+            B3.UseVisualStyleBackColor = true;
+            B3.Click += B3_Click;
+            B3.MouseEnter += button_enter;
+            B3.MouseLeave += button_leave;
+            // 
+            // C1
+            // 
+            C1.Location = new Point(12, 224);
+            C1.Name = "C1";
+            C1.Size = new Size(100, 100);
+            C1.TabIndex = 6;
+            C1.UseVisualStyleBackColor = true;
+            C1.Click += C1_Click;
+            C1.MouseEnter += button_enter;
+            C1.MouseLeave += button_leave;
+            // 
+            // C2
+            // 
+            C2.Location = new Point(118, 224);
+            C2.Name = "C2";
+            C2.Size = new Size(100, 100);
+            C2.TabIndex = 7;
+            C2.UseVisualStyleBackColor = true;
+            C2.Click += C2_Click;
+            C2.MouseEnter += button_enter;
+            C2.MouseLeave += button_leave;
+            // 
+            // C3
+            // 
+            C3.Location = new Point(224, 224);
+            C3.Name = "C3";
+            C3.Size = new Size(100, 100);
+            C3.TabIndex = 8;
+            C3.UseVisualStyleBackColor = true;
+            C3.Click += C3_Click;
+            C3.MouseEnter += button_enter;
+            C3.MouseLeave += button_leave;
+            // 
+            // NewGame
+            // 
+            NewGame.Location = new Point(330, 180);
+            NewGame.Name = "NewGame";
+            NewGame.Size = new Size(114, 44);
+            NewGame.TabIndex = 9;
+            NewGame.Text = "New Game";
+            NewGame.UseVisualStyleBackColor = true;
+            NewGame.Click += NewGame_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(330, 230);
+            button1.Name = "button1";
+            button1.Size = new Size(114, 44);
+            button1.TabIndex = 10;
+            button1.Text = "About";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Red;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(330, 280);
+            button2.Name = "button2";
+            button2.Size = new Size(111, 44);
+            button2.TabIndex = 11;
+            button2.Text = "Exit";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(453, 338);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(NewGame);
+            Controls.Add(C3);
+            Controls.Add(C2);
+            Controls.Add(C1);
+            Controls.Add(B3);
+            Controls.Add(B2);
+            Controls.Add(B1);
+            Controls.Add(A3);
+            Controls.Add(A2);
+            Controls.Add(A1);
+            MaximizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button A1;
+        private Button A2;
+        private Button A3;
+        private Button B1;
+        private Button B2;
+        private Button B3;
+        private Button C1;
+        private Button C2;
+        private Button C3;
+        private Button NewGame;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ Tic Tac Toe/Form1.cs
@@ -0,0 +1,320 @@
+namespace Tic_Tac_Toe
+{
+    public partial class Form1 : Form
+    {
+        bool turn = true;
+        int turn_count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void A1_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (turn)
+            {
+                b.Text = "X";
+            }
+            else
+            {
+                b.Text = "O";
+            }
+
+            turn = !turn;
+            b.Enabled = false;
+            turn_count++;
+
+            checkForWinner();
+        }
+
+        private void checkForWinner()
+        {
+            bool there_is_win = false;
+
+            if ((A1.Text == A2.Text) && (A2.Text == A3.Text) && (!A1.Enabled))
+            {
+                there_is_win = true;
+            }
+            else if ((B1.Text == B2.Text) && (B2.Text == B3.Text) && (!B1.Enabled))
+            {
+                there_is_win = true;
+            }
+            else if ((C1.Text == C2.Text) && (C2.Text == C3.Text) && (!C1.Enabled))
+            {
+                there_is_win = true;
+            }
+            else if ((A1.Text == B1.Text) && (B1.Text == C1.Text) && (!A1.Enabled))
+            {
+                there_is_win = true;
+            }
+            else if ((A2.Text == B2.Text) && (B2.Text == C2.Text) && (!A2.Enabled))
+            {
+                there_is_win = true;
+            }
+            else if ((A3.Text == B3.Text) && (B3.Text == C3.Text) && (!A3.Enabled))
+            {
+                there_is_win = true;
+            }
+
+
+            else if ((A1.Text == B2.Text) && (B2.Text == C3.Text) && (!A1.Enabled))
+            {
+                there_is_win = true;
+            }
+            else if ((A3.Text == B2.Text) && (B2.Text == C1.Text) && (!A3.Enabled))
+            {
+                there_is_win = true;
+            }
+
+
+
+            if (there_is_win)
+            {
+                disableButtons();
+
+                String winner = "";
+                if (turn)
+                {
+                    winner = "O";
+                }
+                else
+                {
+                    winner = "X";
+                }
+
+                MessageBox.Show(winner + " Win!!!");
+            }
+            else
+            {
+                if (turn_count == 9)
+                {
+                    MessageBox.Show("OOP! Try again");
+                }
+            }
+        }
+
+        private void disableButtons()
+        {
+            try
+            {
+                foreach (Control c in Controls)
+                {
+                    Button b = (Button)c;
+                    b.Enabled = false;
+                    NewGame.Enabled = true;
+                    button1.Enabled = true;
+                    button2.Enabled = true;
+                }
+            }
+            catch { }
+        }
+
+        private void A2_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (turn)
+            {
+                b.Text = "X";
+            }
+            else
+            {
+                b.Text = "O";
+            }
+
+            turn = !turn;
+            b.Enabled = false;
+            turn_count++;
+
+            checkForWinner();
+        }
+
+        private void A3_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (turn)
+            {
+                b.Text = "X";
+            }
+            else
+            {
+                b.Text = "O";
+            }
+
+            turn = !turn;
+            b.Enabled = false;
+            turn_count++;
+
+            checkForWinner();
+        }
+
+        private void B1_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (turn)
+            {
+                b.Text = "X";
+            }
+            else
+            {
+                b.Text = "O";
+            }
+
+            turn = !turn;
+            b.Enabled = false;
+            turn_count++;
+
+            checkForWinner();
+        }
+
+        private void B2_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (turn)
+            {
+                b.Text = "X";
+            }
+            else
+            {
+                b.Text = "O";
+            }
+
+            turn = !turn;
+            b.Enabled = false;
+            turn_count++;
+
+            checkForWinner();
+        }
+
+        private void B3_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (turn)
+            {
+                b.Text = "X";
+            }
+            else
+            {
+                b.Text = "O";
+            }
+
+            turn = !turn;
+            b.Enabled = false;
+            turn_count++;
+
+            checkForWinner();
+        }
+
+        private void C1_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (turn)
+            {
+                b.Text = "X";
+            }
+            else
+            {
+                b.Text = "O";
+            }
+
+            turn = !turn;
+            b.Enabled = false;
+            turn_count++;
+
+            checkForWinner();
+        }
+
+        private void C2_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (turn)
+            {
+                b.Text = "X";
+            }
+            else
+            {
+                b.Text = "O";
+            }
+
+            turn = !turn;
+            b.Enabled = false;
+            turn_count++;
+
+            checkForWinner();
+        }
+
+        private void C3_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (turn)
+            {
+                b.Text = "X";
+            }
+            else
+            {
+                b.Text = "O";
+            }
+
+            turn = !turn;
+            b.Enabled = false;
+            turn_count++;
+
+            checkForWinner();
+        }
+
+        private void NewGame_Click(object sender, EventArgs e)
+        {
+            turn = true;
+            turn_count = 0;
+
+            try
+            {
+                foreach (Control c in Controls)
+                {
+                    Button b = (Button)c;
+                    b.Enabled = true;
+                    b.Text = "";
+
+                    NewGame.Text = "New Game";
+                    button1.Text = "About";
+                    button2.Text = "Exit";
+                }
+            }
+            catch { }
+        }
+
+        private void button_enter(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (b.Enabled)
+            {
+                if (turn)
+                {
+                    b.Text = "X";
+                }
+                else
+                {
+                    b.Text = "O";
+                }
+            }
+        }
+        private void button_leave(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (b.Enabled)
+            {
+                b.Text = "";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Thanapon IT02");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
